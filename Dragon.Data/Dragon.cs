e4e1528968317e809f9101a82e83ada526435b5c
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-06-27 15:05:19 -0400

    Database & Connection String

---
+++ Dragon.Data/Dragon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragon.Data
+{
+    public class Dragon
+    {
+        [Key]
+        public int DragonId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [Display(Name = "Your dragon")]
+        public string DragonName { get; set; }
+        [Required]
+        public string DragonColor { get; set; }
+        [Required]
+        public string DragonDescription { get; set; }
+        [Required]
+        public DateTimeOffset BirthOfDragon { get; set; }
+        public DateTimeOffset ModificationOfDragon { get; set; }
+    }
+}
